46d0c6034a122954e0bde6b4b163897a178bc024
Joel <joel.pedersen97@gmail.com>
2019-09-05 15:06:30 +0200

    border for row

--- receptnaring/frontend/src/views/CreateRecipe/CreateRecipe.js
+++ receptnaring/frontend/src/views/CreateRecipe/CreateRecipe.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Container, Row, Col } from 'reactstrap';
 
 const CreateRecipe = () => {
 
+  const StyledRow = styled(Row)`
+    border-bottom: 1px solid #707070;
+  `;
+
   return (
     <Container>
-      <Row>
+      <StyledRow>
         <Col md="6" sm="12">HELLO</Col>
         <Col md="6" sm="12">HELLO</Col>
-      </Row>
+      </StyledRow>
       <Row>
         <Col md="6" sm="12">HELLO</Col>
         <Col md="6" sm="12">HELLO</Col>
