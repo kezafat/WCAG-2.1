2f7e25fbb06be9b71d542f830f0b6bb1ff5ce8cc
Joel <joel.pedersen97@gmail.com>
2019-09-05 17:15:51 +0200

    small change

--- receptnaring/frontend/src/views/CreateRecipe/CreateRecipe.js
+++ receptnaring/frontend/src/views/CreateRecipe/CreateRecipe.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Container, Row, Col } from 'reactstrap';
+import { StyledRow } from './StyledCreateRecipe';
 
 const CreateRecipe = () => {
 
-  const StyledRow = styled(Row)`
-    border-bottom: 1px solid #707070;
-  `;
-
   return (
     <Container>
-      <StyledRow>
+      <StyledRow className="text-center">
         <Col md="6" sm="12">HELLO</Col>
         <Col md="6" sm="12">HELLO</Col>
       </StyledRow>
-      <Row>
+      <Row className="text-center">
         <Col md="6" sm="12">HELLO</Col>
         <Col md="6" sm="12">HELLO</Col>
       </Row>
---
+++ receptnaring/frontend/src/views/CreateRecipe/StyledCreateRecipe.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import { Row } from 'reactstrap';
+
+export const StyledRow = styled(Row)`
+border-bottom: 1px solid #707070;
+`;
