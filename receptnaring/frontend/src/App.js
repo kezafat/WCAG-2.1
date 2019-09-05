8cb46f65d220d8212e5aef01e3d68f657448bf76
Joel <joel.pedersen97@gmail.com>
2019-09-05 14:24:34 +0200

    rows for create recipe page

--- receptnaring/frontend/src/App.js
+++ receptnaring/frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CreateRecipe from './views/CreateRecipe';
 
 function App() {
   return (
---
+++ receptnaring/frontend/src/views/CreateRecipe/CreateRecipe.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+const CreateRecipe = () => {
+
+  return (
+    <Container>
+      <Row>
+        <Col md="6">HELLO</Col>
+        <Col md="6">HELLO</Col>
+      </Row>
+      <Row>
+        <Col md="6">HELLO</Col>
+        <Col md="6">HELLO</Col>
+      </Row>
+    </Container>
+  )
+};
+
+export default CreateRecipe;
