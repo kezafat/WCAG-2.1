15a08b893f8c91ff871d33cfc29f11c528da8c39
Joel <joel.pedersen97@gmail.com>
2019-09-09 14:37:29 +0200

    awesome code for title person time etc

--- receptnaring/frontend/src/views/CreateRecipe/components/TitlePersonTime/StyledTitlePersonTime.js
+++ receptnaring/frontend/src/views/CreateRecipe/components/TitlePersonTime/StyledTitlePersonTime.js
@@ -1,32 +1,57 @@
 import styled from 'styled-components';
 
 export const StyledContainer = styled.section`
-
+  width: 300px;
+  margin: 0 auto;
 `;
 
 export const StyledInputGroup = styled.section`
   display: flex;
-  margin: 1rem 0;
+  align-items: center;
+  justify-content: space-between;
+  margin: 2rem 0;
 `;
 
 export const StyledH2 = styled.h2`
   color: #707070;
-  margin: 0 1rem 0 0;
+  font-size: 1.6rem;
+  margin-right: 1rem;
 `;
 
 export const StyledTitleInput = styled.input`
   border: none;
   border-bottom: 1px solid #707070;
+  padding: 0 0.5rem;
+  width: 100%;
 `;
 
 export const StyledH3 = styled.h3`
-  font-size: 
+  color: #707070;
+  font-size: 1.2rem;
 `;
 
-export const StyledDraggableInput = styled.div`
+export const StyledSliderInputContainer = styled.div`
+  width: 13rem;
+`;
 
+export const StyledSliderInput = styled.input`
+  appearance: none;
+  background: lightgrey;
+  height: 1rem;
+  width: 100%;
+  border-radius: 3px;
+  &::-webkit-slider-thumb {
+    appearance: none;
+    width: 1rem;
+    height: 1rem;
+    background: #000;
+    border-radius: 50%;
+    cursor: pointer;
+  }
 `;
 
 export const StyledInput = styled.input`
-
+  width: 3rem;
+  text-align: center;
+  margin: ${props => props.primary ? '0 1rem 0 auto ' : '0 auto 0 0'}; 
 `;
--- receptnaring/frontend/src/views/CreateRecipe/components/TitlePersonTime/TitlePersonTime.js
+++ receptnaring/frontend/src/views/CreateRecipe/components/TitlePersonTime/TitlePersonTime.js
@@ -1,7 +1,36 @@
-import React from 'react';
-import { StyledContainer, StyledInputGroup, StyledH2, StyledTitleInput, StyledH3, StyledDraggableInput, StyledInput } from './StyledTitlePersonTime';
+import React, { useState } from 'react';
+import { StyledContainer, StyledInputGroup, StyledH2, StyledTitleInput, StyledH3, StyledSliderInputContainer, StyledSliderInput, StyledInput } from './StyledTitlePersonTime';
+import { sliderInputsData } from '../../../../staticData';
 
 const TitlePersonTime = () => {
+  const [sliderValues, setSliderValues] = useState({
+    people: 0,
+    hours: 0,
+    minutes: 0
+  });
+  /* const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0); */
+
+  const renderSliders = () => sliderInputsData.map(({ title, name, min, max, id }) =>
+    <StyledInputGroup key={id}>
+      <StyledH3>
+        {title}
+      </StyledH3>
+      <StyledSliderInputContainer>
+        <StyledSliderInput type="range" name={name} min={min} max={max} defaultValue={sliderValues[name]} onChange={onSliderChange} />
+      </StyledSliderInputContainer>
+    </StyledInputGroup>
+  );
+
+  // const onSliderChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  const onSliderChange = e => {
+    e.persist();
+    console.log(e.target.getAttribute('name'));
+    console.log(e.target.value);
+    setSliderValues(sliderValues => ({ ...sliderValues, [e.target.getAttribute('name')]: e.target.value }));
+  }
+
   return (
     <StyledContainer>
       <StyledInputGroup>
@@ -10,27 +39,10 @@ const TitlePersonTime = () => {
         </StyledH2>
         <StyledTitleInput />
       </StyledInputGroup>
+      {renderSliders()}
       <StyledInputGroup>
-        <StyledH3>
-          Personer
-        </StyledH3>
-        <StyledDraggableInput />
-      </StyledInputGroup>
-      <StyledInputGroup>
-        <StyledH3>
-          Timmar
-        </StyledH3>
-        <StyledDraggableInput />
-      </StyledInputGroup>
-      <StyledInputGroup>
-        <StyledH3>
-          Minuter
-        </StyledH3>
-        <StyledDraggableInput />
-      </StyledInputGroup>
-      <StyledInputGroup>
-        <StyledInput />
-        <StyledInput />
+        <StyledInput type="number" placeholder="h" min="0" value={sliderValues.hours} readOnly primary />
+        <StyledInput type="number" placeholder="min" min="0" />
       </StyledInputGroup>
     </StyledContainer>
   )
