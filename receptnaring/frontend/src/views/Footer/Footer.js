b04d2f257bdab7d9831cf83aa63f365b1d387d20
Tommy <tommynguyen@live.se>
2019-09-11 10:02:43 +0200

    subrender funktion

--- receptnaring/frontend/src/views/Footer/Footer.js
+++ receptnaring/frontend/src/views/Footer/Footer.js
@@ -4,28 +4,38 @@ import { FooterRow, IconText, StyledCol, StyledImage, ContactText, ContactCol } 
 
 class Footer extends Component {
 
+  footerIconList = [
+    {
+      name: 'Facebook',
+      src: './images/facebook.svg',
+      id: 0
+    },
+    {
+      name: 'Instagram',
+      src: './images/instagram-logo.svg',
+      id: 1
+    },
+    {
+      name: 'Twitter',
+      src: './images/twitter-logo-button.svg',
+      id: 2
+    }
+  ];
+
+  renderIcons = () => this.footerIconList.map(({ name, src, id }) =>
+    <StyledCol key={id} lg="4">
+      <StyledImage src= {src} />
+      <IconText>
+        {name}
+    </IconText>
+    </StyledCol>
+  );
+
   render() {
     return (
       <Container>
         <FooterRow>
-          <StyledCol lg="4">
-            <StyledImage src="./images/facebook.svg" />
-            <IconText>
-              Facebook
-              </IconText>
-          </StyledCol>
-          <StyledCol lg="4">
-            <StyledImage src="./images/instagram-logo.svg" />
-            <IconText>
-              Instagram
-              </IconText>
-          </StyledCol>
-          <StyledCol lg="4">
-            <StyledImage src="./images/twitter-logo-button.svg" />
-            <IconText>
-              Twitter
-              </IconText>
-          </StyledCol>
+          {this.renderIcons()}
           <ContactCol lg="12">
             <ContactText>
               <a href="mailto:info@receptnaring.se">info@receptnaring.se</a>
