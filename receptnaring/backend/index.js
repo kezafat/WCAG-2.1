578845fa8fb73056cead1a81aadbf3eef92fd5b3
SimonPalmgren <simon.palmgren91@gmail.com>
2019-09-11 15:40:36 +0200

    file changes (imports, removals etc)

--- receptnaring/backend/index.js
+++ receptnaring/backend/index.js
@@ -2,10 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const connectToDb = require('./config/db');
 const config = require('./config/config');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
 const recipeRoutes = require('./API/recipeRoutes');
-const settings = require('./config/settings.json');
+const cors = require('cors');
 
 connectToDb();
 
@@ -14,14 +12,7 @@ const app = express();
 app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('Välkommen till Recept & Närings super server'));
 
-app.use(session({
-  secret: settings.cookieSecret,
-  resave: true,
-  saveUninitialized: true,
-  store: new MongoStore({
-    mongooseConnection: global.db
-  })
-}));
+app.use(cors())
 
 app.use(recipeRoutes);
 app.listen(config.PORT, () => console.log(`Gulligagruppens server is on port ${config.PORT}`));
