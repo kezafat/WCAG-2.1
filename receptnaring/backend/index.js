4a8da1eca9dfa40cb4d864af56c2a6ce7cfe418f
Hamsho <hamidjade@gmail.com>
2019-09-12 14:46:25 +0200

    Added dependencies and removed sessions!

--- receptnaring/backend/index.js
+++ receptnaring/backend/index.js
@@ -2,9 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const connectToDb = require('./config/db');
 const config = require('./config/config');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
 const recipeRoutes = require('./API/recipeRoutes');
+const dbIngRoute = require('./API/dbIngRoute');
 const settings = require('./config/settings.json');
 
 connectToDb();
@@ -14,14 +13,6 @@ const app = express();
 app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('Välkommen till Recept & Närings super server'));
 
-app.use(session({
-  secret: settings.cookieSecret,
-  resave: true,
-  saveUninitialized: true,
-  store: new MongoStore({
-    mongooseConnection: global.db
-  })
-}));
-
 app.use(recipeRoutes);
+app.use(dbIngRoute);
 app.listen(config.PORT, () => console.log(`Gulligagruppens server is on port ${config.PORT}`));
