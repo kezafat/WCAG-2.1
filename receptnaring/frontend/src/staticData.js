2ad0e332a847565d7e673cc5afaf52fd1af9f09a
TommyNguyen95 <43949227+TommyNguyen95@users.noreply.github.com>
2019-09-14 10:22:46 +0200

    Tommy/footercomponent (#6)
    * deleted files
    
    * added files.js
    
    * edit file
    
    * yes
    
    * fix
    
    * Footer done
    
    * Final Footer
    
    * Fixat footern samt allmänna färger på hemsidan (för att färger ska läggas på rätt ställe)
    
    * subrender funktion
    
    * Import data from staticdata

--- receptnaring/frontend/src/staticData.js
+++ receptnaring/frontend/src/staticData.js
@@ -25,6 +25,25 @@ const sliderInputsData = [
   },
 ];
 
+const footerIconList = [
+  {
+    name: 'Facebook',
+    src: './images/facebook.svg',
+    id: 0
+  },
+  {
+    name: 'Instagram',
+    src: './images/instagram-logo.svg',
+    id: 1
+  },
+  {
+    name: 'Twitter',
+    src: './images/twitter-logo-button.svg',
+    id: 2
+  }
+];
+
 export {
-  sliderInputsData
+  sliderInputsData,
+  footerIconList
 }
