e76b1883dc73fced994bde3767334cab664c68e3
SimonPalmgren <simon.palmgren91@gmail.com>
2019-09-15 23:11:13 +0200

    some changes to sorting and added substring function + rendering

--- receptnaring/frontend/src/components/SortedRecipes/index.js
+++ receptnaring/frontend/src/components/SortedRecipes/index.js
@@ -1,44 +1,58 @@
-import React, { useEffect, useState } from 'react'
-import { SortedRecipesWrapper, StyledCard, StyledCardBody, StyledCardTitle, StyledCardImg, StyledSpinner } from './StyleSortedRecipes'
+import React, { useEffect, useState } from 'react';
+import { SortedRecipesWrapper, StyledCard, StyledCardBody, StyledCardTitle, StyledCardImg, StyledSpinner, StyledH3, StyledLink } from './StyleSortedRecipes';
+import { Col, Row } from 'reactstrap'
 
 
 function SortedRecipes() {
 
-    const useFetch = (url) => {
-        const [data, setData] = useState([]);
-        const [loading, setLoading] = useState(true);
+  const useFetch = (url) => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-        useEffect(() => {
-            const fetchData = async () => {
-                const respons = await fetch(url);
-                const data = await respons.json();
-                const items = data;
-                const slicedData = items.slice(-3);
-                setData(slicedData);
-                setLoading(false);
-            };
-            fetchData();
-        }, [url]);
-        return { data, loading };
-    }
-    const { data, loading } = useFetch('http://localhost:3001/api/recipes');
+    useEffect(() => {
+      const fetchData = async () => {
+        const respons = await fetch(url);
+        const data = await respons.json();
+        const items = data;
+        const slicedData = items.slice(-3);
+        const reverseData = slicedData.reverse();
+        setData(reverseData);
+        setLoading(false);
+      };
+      fetchData();
+    }, [url]);
+    return { data, loading };
+  }
+  const { data, loading } = useFetch('http://localhost:3001/api/recipes');
 
-    const renderRecipe = (data, index) => <StyledCard key={index} >
-        <StyledCardBody>
-            <StyledCardImg src={`/images/${data.img}`}></StyledCardImg>
-            <StyledCardTitle>{data.title}</StyledCardTitle>
-        </StyledCardBody>
-    </StyledCard>
+  const renderRecipe = (data, index) => <Col lg="4" key={index}><StyledCard >
+    <StyledLink to={"/recept/" + data._id}>
+    <StyledCardBody>
+      <StyledCardImg src={`/images/${data.img}`}></StyledCardImg>
+      <StyledCardTitle>{trimString(data.title, 24)}</StyledCardTitle>
+    </StyledCardBody>
+    </StyledLink>
+  </StyledCard>
+  </Col>
 
-    const renderRecipes = () => data.map((simon, index) => renderRecipe(simon, index))
+  const renderRecipes = () => data.map((item, index) => renderRecipe(item, index));
 
-    return (
-        <SortedRecipesWrapper>
-            { loading ? (<StyledSpinner/>) :
-            (renderRecipes(data))
+  const trimString = function (string, length) {
+    return string.length > length ? 
+           string.substring(0, length) + '...' :
+           string;
+  };
+
+  return (
+    <SortedRecipesWrapper>
+      <StyledH3>Senaste Recept</StyledH3>
+      <Row>
+        {loading ? (<StyledSpinner />) :
+          (renderRecipes(data))
         }
-        </SortedRecipesWrapper>
-    );
+      </Row>
+    </SortedRecipesWrapper>
+  );
 };
 
 export default SortedRecipes
