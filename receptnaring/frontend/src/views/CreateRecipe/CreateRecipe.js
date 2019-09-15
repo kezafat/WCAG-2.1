9d8a74c618c5ab3f7a59de269eaf17a00ae2c765
Hamsho <hamidjade@gmail.com>
2019-09-15 09:30:16 +0200

    Added img upload and adjusted api routes + parent component (state is lifted up)

--- receptnaring/frontend/src/views/CreateRecipe/CreateRecipe.js
+++ receptnaring/frontend/src/views/CreateRecipe/CreateRecipe.js
@@ -1,29 +1,31 @@
-import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import React, { useState, useContext } from 'react';
+import { Container, Row, Col, Button } from 'reactstrap';
 import { StyledRow } from './StyledCreateRecipe';
 import UploadImage from './components/UploadImage';
 import TitlePersonTime from './components/TitlePersonTime';
 
-class CreateRecipe extends Component {
+const CreateRecipe = () => {
 
-  render() {
-    return (
-      <Container>
-        <StyledRow className="text-center">
-          <Col md="6" sm="12">
-            <UploadImage />
-          </Col>
-          <Col md="6" sm="12">
-            <TitlePersonTime />
-          </Col>
-        </StyledRow>
-        <Row className="text-center">
-          <Col md="6" sm="12">HELLO</Col>
-          <Col md="6" sm="12">HELLO</Col>
-        </Row>
-      </Container>
-    )
-  }
+  const [state, setState] = useState({
+    'img': 'upload-image.png'
+  })
+
+  return (
+    <Container>
+      <StyledRow className="text-center">
+        <Col md="6" sm="12">
+          <UploadImage s={[state, setState]} />
+        </Col>
+        <Col md="6" sm="12">
+          <TitlePersonTime s={[state, setState]} />
+        </Col>
+      </StyledRow>
+      <Row className="text-center">
+        <Col md="6" sm="12">HELLO</Col>
+        <Col md="6" sm="12">HELLO</Col>
+      </Row>
+    </Container>
+  )
 };
 
 export default CreateRecipe;
--- receptnaring/frontend/src/views/CreateRecipe/components/UploadImage/UploadImage.js
+++ receptnaring/frontend/src/views/CreateRecipe/components/UploadImage/UploadImage.js
@@ -1,23 +1,41 @@
-import React, { useState } from 'react';
-import {
-  StyledUploadContainer,
-  StyledUploadInput,
-  StyledUploadLabel
-} from './StyledUploadImage';
-import image from '../../../../assets/images/upload-image.png';
+import React, { useState, useContext } from 'react';
+import { StyledUploadContainer, StyledUploadInput, StyledUploadLabel } from './StyledUploadImage';
 
-const UploadImage = () => {
-  const [uploadedImage, setUploadedImage] = useState({});
+const axios = require("axios");
+const uniqid = require('uniqid');
 
-  const addImage = e => setUploadedImage(e.target.files[0]);
+
+
+const UploadImage = (ctx) => {
+  const [get, set] = ctx.s;
+  const imgPath = `/images/uploaded/${get.img}`;
+  const imgName = uniqid();
+
+  const addImage = async e => {
+    let file = e.target.files[0];
+    const formData = new FormData();
+    formData.append(imgName, file);
+
+    axios.post("http://localhost:3001/api/uploadimg", formData, {
+      'content-type': 'multipart/form-data'
+    })
+      .then((response) => {
+        set(prev => ({ ...prev, img: response.data.filename }))
+      }).catch((error) => {
+        //No need for errorhandling at the moment
+      });
+  };
+
 
   return (
     <StyledUploadContainer>
-      <img src={image} alt="Bild på en vit kamera med blå bakgrund" />
-      <StyledUploadInput onChange={addImage} type="file" name="upload" id="upload" />
-      <StyledUploadLabel htmlFor="upload">
-        Lägg upp ett foto
+      <form>
+        <img src={(imgPath)} alt="Bild på en vit kamera med blå bakgrund" />
+        <StyledUploadInput onChange={addImage} type="file" name="upload" id="upload" />
+        <StyledUploadLabel htmlFor="upload">
+          Välj bild för maträtt
       </StyledUploadLabel>
+      </form>
     </StyledUploadContainer>
   )
 }
