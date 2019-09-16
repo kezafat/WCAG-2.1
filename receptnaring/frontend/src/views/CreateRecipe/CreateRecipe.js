e276e17832be61169bee29e61f3b746a72a38662
Hamsho <hamidjade@gmail.com>
2019-09-16 08:26:29 +0200

    WIP - sendforms

--- receptnaring/frontend/src/views/CreateRecipe/CreateRecipe.js
+++ receptnaring/frontend/src/views/CreateRecipe/CreateRecipe.js
@@ -4,19 +4,35 @@ import { Container, Row, Col, Button } from 'reactstrap';
 import { StyledRow } from './StyledCreateRecipe';
 import UploadImage from './components/UploadImage';
 import TitlePersonTime from './components/TitlePersonTime';
+const axios = require("axios");
 
 const CreateRecipe = () => {
   const initState = { 'img': 'upload-image.png' }
   const [state, setState] = useState(initState)
+  const [formstate, setFormstate] = useState({ 'status': false })
 
   const clearForms = () => {
     setState({ ...initState, go: "/" });
   }
 
+  const sendForms = () => {
+    axios.post("http://localhost:3001/api/recipe", state)
+      .then((res) => {
+        console.log("res", res)
+        if (res.data.name == "ValidationError") {
+          console.log(res.data.message)
+          return;
+        }
+        setFormstate({ 'status': true })
+      }).catch((error) => {
+        //No need for errorhandling at the moment
+      });
+  }
 
   return (
     <Container>
       {state.go && <Redirect to="/"></Redirect>}
+      {formstate.status ? "formOK" : "formBAD"}
       <StyledRow className="text-center">
         <Col md="6" sm="12">
           <UploadImage s={[state, setState]} />
@@ -31,7 +47,7 @@ const CreateRecipe = () => {
       </Row>
       <Row className="text-center d-flex justify-content-around mt-4">
         <Button color="danger" size="lg" onClick={clearForms}>Avbryt</Button>
-        <Button color="success" size="lg">Spara</Button>
+        <Button color="success" size="lg" onClick={sendForms}>Spara</Button>
       </Row>
     </Container>
   )
