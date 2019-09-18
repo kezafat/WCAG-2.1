80aad5a9f44d545a5e38fa8be2e365ba90e9229f
Tommy <tommynguyen@live.se>
2019-09-18 16:19:09 +0200

    Remake of footer

--- receptnaring/frontend/src/views/Footer/StyledFooter.js
+++ receptnaring/frontend/src/views/Footer/StyledFooter.js
@@ -1,37 +1,31 @@
 import styled from 'styled-components'
-import { Row, Col } from 'reactstrap'
+import { Col } from 'reactstrap'
 
-export const FooterRow = styled(Row)`
+
+export const FooterRow = styled.footer`
     background-color: #EFE9E6;
-    align-items: center;
     padding-top: 30px;
     margin-top: 30px;
     border-top: 1px solid #eadad2;
 `;
 
 export const IconText = styled.a`
-    font-size: 26px;
-    display: inline-block;
-    margin: 0;
-    margin-left: 10px;
-    vertical-align: middle;
-    font-family: 'Open Sans', sans-serif;
+  font-size: 26px;
+  display: inline-block;
+  margin: 0;
+  margin-left: 10px;
+  vertical-align: middle;
+  font-family: 'Open Sans', sans-serif;
 `;
 
 export const StyledCol = styled(Col)`
-    text-align: center;
-`;
-
-export const ContactCol = styled(Col)`
-    text-align: center;
+  text-align: center;
 `;
 
 export const StyledImage = styled.img`
-    height: 36px;
-    padding-right: 15px;
+  height: 36px;
 `;
 
-export const ContactText = styled.p`
-    font-size: 24px;
-    margin: 20px 20px;
+export const ContactText = styled.a`
+  font-size: 24px;
 `;
