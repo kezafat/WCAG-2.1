9985bd602991b80397fd94498c661f24e8e02510
Tommy <tommynguyen@live.se>
2019-09-18 09:38:27 +0200

    fixed merge conflic

--- receptnaring/frontend/src/staticData.js
+++ receptnaring/frontend/src/staticData.js
@@ -15,29 +15,4 @@ export const footerIconList = [
     id: 2
   }
 ];
-<<<<<<< HEAD
 
-const footerIconList = [
-  {
-    name: 'Facebook',
-    src: './images/facebook.svg',
-    id: 0
-  },
-  {
-    name: 'Instagram',
-    src: './images/instagram-logo.svg',
-    id: 1
-  },
-  {
-    name: 'Twitter',
-    src: './images/twitter-logo-button.svg',
-    id: 2
-  }
-];
-
-export {
-  sliderInputsData,
-  footerIconList
-}
-=======
->>>>>>> dev
